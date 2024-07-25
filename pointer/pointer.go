ac3c62b34f59913d36126b0f3179893738baa811
Ilya Uglov <iauglov@gmail.com>
2024-07-25 19:52:43 +0400

    Add value or defaule method to pointer

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -4,10 +4,17 @@ func To[T any](val T) *T {
 	return &val
 }
 
-func Of[T any](val *T) T {
+func ValueOf[T any](val *T) T {
 	if val != nil {
 		return *val
 	}
 	var def T
 	return def
+}
+
+func ValueOrDefault[T any](val *T, defValue T) T {
+	if val != nil {
+		return *val
+	}
+	return defValue
 }
