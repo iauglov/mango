29c563b9e903ee2fdc618a33814bd1dcedf6e770
Ilya Uglov <iauglov@gmail.com>
2024-11-24 22:32:23 +0400

    Fix cache naming

--- sync_cahce.go
+++ sync_cahce.go
@@ -2,32 +2,32 @@ package mango
 
 import "sync"
 
-type SyncMemCahce[K comparable, V any] struct {
+type SyncMemCache[K comparable, V any] struct {
 	cache map[K]V
 	mu    *sync.RWMutex
 }
 
-func NewSyncCache[K comparable, V any]() *SyncMemCahce[K, V] {
-	return &SyncMemCahce[K, V]{
+func NewSyncCache[K comparable, V any]() *SyncMemCache[K, V] {
+	return &SyncMemCache[K, V]{
 		cache: make(map[K]V),
 		mu:    &sync.RWMutex{},
 	}
 }
 
-func (c *SyncMemCahce[K, V]) Get(key K) (V, bool) {
+func (c *SyncMemCache[K, V]) Get(key K) (V, bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	v, ok := c.cache[key]
 	return v, ok
 }
 
-func (c *SyncMemCahce[K, V]) Put(key K, val V) {
+func (c *SyncMemCache[K, V]) Put(key K, val V) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.cache[key] = val
 }
 
-func (c *SyncMemCahce[K, V]) Delete(key K) {
+func (c *SyncMemCache[K, V]) Delete(key K) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	delete(c.cache, key)
